cc9c0cc903ecb761da9a29750e231d0a0922977f
SHRUTI TALUKDER <99132542+shrutitalukder@users.noreply.github.com>
2022-06-14 19:33:59 +0530

    Create Wave Array.cpp

---
+++ Array/GeeksForGeeks/Wave Array.cpp
@@ -0,0 +1,18 @@
+//Given a sorted array arr[] of distinct integers. Sort the array into a wave-like array(In Place).
+//In other words, arrange the elements into a sequence such that arr[1] >= arr[2] <= arr[3] >= arr[4] <= arr[5].....
+
+//If there are multiple solutions, find the lexicographically smallest one.
+
+//Your Task:
+//The task is to complete the function convertToWave(), which converts the given array to a wave array.
+//NOTE: The given array is sorted in ascending order, and you don't need to return anything to make changes in the original array itself.
+
+//Expected Time Complexity: O(n).
+//Expected Auxiliary Space: O(1).
+
+//Constraints:
+//1 ≤ n ≤ 106
+//0 ≤ arr[i] ≤107
+
+//Solution
+#include<iostream>
