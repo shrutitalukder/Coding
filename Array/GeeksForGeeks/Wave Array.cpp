76350edd8c09c9c049325b7d0c8bd9e98f05a861
SHRUTI TALUKDER <99132542+shrutitalukder@users.noreply.github.com>
2022-06-16 09:28:11 +0530

    Update Wave Array.cpp

--- Array/GeeksForGeeks/Wave Array.cpp
+++ Array/GeeksForGeeks/Wave Array.cpp
@@ -14,5 +14,61 @@
 //1 ≤ n ≤ 106
 //0 ≤ arr[i] ≤107
 
+//Example 1
+//Input:
+//n = 5
+//arr[] = {1,2,3,4,5}
+//Output: 2 1 4 3 5
+//Explanation: Array elements after 
+//sorting it in wave form are 
+//2 1 4 3 5.
+
+//Example 2
+//Input:
+//n = 6
+//arr[] = {2,4,7,8,9,10}
+//Output: 4 2 8 7 10 9
+//Explanation: Array elements after 
+//sorting it in wave form are 
+//4 2 8 7 10 9.
+
 //Solution
 #include<iostream>
+using namespace std;
+class solution{
+  public:
+  void convertToWave(int n,vector<int>& ar){
+    if(n%2==0){
+      int temp;
+      for(int i=0;i<n;i+=2){
+        temp=ar[i];
+        ar[i]=ar[i+1];
+        ar[i+1]=temp;
+      }
+    }
+    else{
+      for(int i=0;i<n-1;i+=2){
+        int temp;
+        temp=ar[i];
+        ar[i]=ar[i+1];
+        ar[i+1]=temp;
+      }
+    }
+  }
+  int main(){
+    int t,n;
+    cin>>t;                    //input testcases
+    while(t--){                //while testcases exist
+      cin>>n;                  //input size of array
+      vector<int>a(n);         //declare vector of size n
+      for(int i=0;i<n;i++){
+        cin>>a[i];             //input elements of array
+      }
+      solution ob;
+      ob.convertToWave(n,a);
+      for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";       //print array
+      }
+      cout<<endl;
+    }
+  }
